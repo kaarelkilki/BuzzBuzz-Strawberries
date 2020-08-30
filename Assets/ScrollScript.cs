08a167faa82cae4d889bbe12192c12fa747c89ac
DESKTOP-D980RR6\talu <kaarel.kilki@gmail.com>
2020-08-30 07:38:27 +0300

    Clouds

---
+++ Assets/ScrollScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    public float scrollSpeed = -5f;
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 40);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+}
