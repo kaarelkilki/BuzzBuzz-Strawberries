160371eaf0ed6c3f241524b78f32132fb358256b
DESKTOP-D980RR6\talu <kaarel.kilki@gmail.com>
2020-09-09 15:54:32 +0300

    Collectables and score

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Bee : MonoBehaviour
 {
     // Movement speed
     public float speed = 2;
-
     // Flap force
     public float force = 300;
+    
+    public Text countText;
+
+    // Score count
+    private int count;
 
     // Use this for initialization
     void Start()
     {
         // Fly towards the right
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        count = 0;
+        SetScoreText();
     }
 
     // Update is called once per frame
@@ -26,9 +34,24 @@ public class Bee : MonoBehaviour
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetScoreText();
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         // Restart
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void SetScoreText ()
+    {
+        countText.text = "SCORE: " + count.ToString();
     }
 }
