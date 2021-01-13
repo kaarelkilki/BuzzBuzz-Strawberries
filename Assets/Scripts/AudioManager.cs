9b201625edf2963d678878efecbbe0e1514923b3
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2021-01-13 15:40:18 +0200

    InstructionsImage PENDING

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
---
+++ Assets/Scripts/FadeAway.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAway : MonoBehaviour
+{
+    // the image you want to fade, assign in inspector
+    public Image imgUp;
+
+    void Start()
+    {
+        // fades the image out on start
+        if(GameObject.Find("CanvasPlay").activeInHierarchy == true)
+        {
+            StartCoroutine(FadeImage(true));
+        }
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 3 second backwards
+            for (float i = 4; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                imgUp.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+    }
+}
